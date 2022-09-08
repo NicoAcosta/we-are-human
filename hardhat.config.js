eb30493b6453e49980838b3b85a11ab2faa1c913
Nicolás Acosta <nicacosta@frba.utn.edu.ar>
2022-09-07 23:43:22 -0300

    add abi

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,14 @@
-require("@nomicfoundation/hardhat-toolbox");
+require('@nomicfoundation/hardhat-toolbox')
+require('hardhat-abi-exporter')
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
-  solidity: "0.8.9",
-};
+	solidity: '0.8.9',
+	abiExporter: {
+		path: './abi',
+		runOnCompile: true,
+		flat: true,
+		spacing: 2,
+		pretty: true
+	}
+}
